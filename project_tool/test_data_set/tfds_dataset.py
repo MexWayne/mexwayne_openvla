5545ef9cb60e99ce0f1c1238861ee7c0409a15dc
mex_wayne <mex_wyane@163.com>
2025-06-23 12:56:03 +0800

    [NEW][project_tool]: 1 commit the project tool for visualize the dataset 2 the download is not ready

--- project_tool/test_data_set/tfds_dataset.py
+++ project_tool/test_data_set/tfds_dataset.py
@@ -97,17 +97,19 @@ def show_dataset_size():
 
 
 def download_dataset():
-    #print(f"Downloading {len()} datasets to {DOWNLOAD_DIR}.")
-    #for dataset_name in tqdm.tqdm(DATASET_NAMES):
-    #    _ = tfds.load(dataset_name, data_dir=DOWNLOAD_DIR)
+    print(f"Downloading {len(DATASET_NAMES)} datasets to {DOWNLOAD_DIR}.")
+    for dataset_name in tqdm.tqdm(DATASET_NAMES):
+        _ = tfds.load(dataset_name, data_dir=DOWNLOAD_DIR)
 
-    # 获取 builder，但不自动 prepare
-    dataset_name = "fractal20220817_data"
-    builder = tfds.builder(dataset_name, data_dir=DOWNLOAD_DIR)
-    # 手动构建数据（下载 + 解压 + 构建），禁用 Beam 默认并发，防止 recursion 错误
-    builder.download_and_prepare(try_download=False, try_gcs=False)
-    # 加载 tf.data.Dataset
-    ds = builder.as_dataset(split="train")
+
+
+    ## 获取 builder，但不自动 prepare
+    #dataset_name = "fractal20220817_data"
+    #builder = tfds.builder(dataset_name, data_dir=DOWNLOAD_DIR)
+    ## 手动构建数据（下载 + 解压 + 构建），禁用 Beam 默认并发，防止 recursion 错误
+    #builder.download_and_prepare(try_gcs=False)
+    ## 加载 tf.data.Dataset
+    #ds = builder.as_dataset(split="train")
 
 
 
@@ -117,7 +119,7 @@ def visiual_dataset():
     # choose the dataset path in the dropdown on the right and rerun this cell
     # to see multiple samples
     
-    dataset = 'fractal20220817_data' # @param ['fractal20220817_data', 'kuka', 'bridge', 'taco_play', 'jaco_play', 'berkeley_cable_routing', 'roboturk', 'nyu_door_opening_surprising_effectiveness', 'viola', 'berkeley_autolab_ur5', 'toto', 'language_table', 'columbia_cairlab_pusht_real', 'stanford_kuka_multimodal_dataset_converted_externally_to_rlds', 'nyu_rot_dataset_converted_externally_to_rlds', 'stanford_hydra_dataset_converted_externally_to_rlds', 'austin_buds_dataset_converted_externally_to_rlds', 'nyu_franka_play_dataset_converted_externally_to_rlds', 'maniskill_dataset_converted_externally_to_rlds', 'furniture_bench_dataset_converted_externally_to_rlds', 'cmu_franka_exploration_dataset_converted_externally_to_rlds', 'ucsd_kitchen_dataset_converted_externally_to_rlds', 'ucsd_pick_and_place_dataset_converted_externally_to_rlds', 'austin_sailor_dataset_converted_externally_to_rlds', 'austin_sirius_dataset_converted_externally_to_rlds', 'bc_z', 'usc_cloth_sim_converted_externally_to_rlds', 'utokyo_pr2_opening_fridge_converted_externally_to_rlds', 'utokyo_pr2_tabletop_manipulation_converted_externally_to_rlds', 'utokyo_saytap_converted_externally_to_rlds', 'utokyo_xarm_pick_and_place_converted_externally_to_rlds', 'utokyo_xarm_bimanual_converted_externally_to_rlds', 'robo_net', 'berkeley_mvp_converted_externally_to_rlds', 'berkeley_rpt_converted_externally_to_rlds', 'kaist_nonprehensile_converted_externally_to_rlds', 'stanford_mask_vit_converted_externally_to_rlds', 'tokyo_u_lsmo_converted_externally_to_rlds', 'dlr_sara_pour_converted_externally_to_rlds', 'dlr_sara_grid_clamp_converted_externally_to_rlds', 'dlr_edan_shared_control_converted_externally_to_rlds', 'asu_table_top_converted_externally_to_rlds', 'stanford_robocook_converted_externally_to_rlds', 'eth_agent_affordances', 'imperialcollege_sawyer_wrist_cam', 'iamlab_cmu_pickup_insert_converted_externally_to_rlds', 'uiuc_d3field', 'utaustin_mutex', 'berkeley_fanuc_manipulation', 'cmu_food_manipulation', 'cmu_play_fusion', 'cmu_stretch', 'berkeley_gnm_recon', 'berkeley_gnm_cory_hall', 'berkeley_gnm_sac_son']
+    dataset = 'bc_z' # @param ['fractal20220817_data', 'kuka', 'bridge', 'taco_play', 'jaco_play', 'berkeley_cable_routing', 'roboturk', 'nyu_door_opening_surprising_effectiveness', 'viola', 'berkeley_autolab_ur5', 'toto', 'language_table', 'columbia_cairlab_pusht_real', 'stanford_kuka_multimodal_dataset_converted_externally_to_rlds', 'nyu_rot_dataset_converted_externally_to_rlds', 'stanford_hydra_dataset_converted_externally_to_rlds', 'austin_buds_dataset_converted_externally_to_rlds', 'nyu_franka_play_dataset_converted_externally_to_rlds', 'maniskill_dataset_converted_externally_to_rlds', 'furniture_bench_dataset_converted_externally_to_rlds', 'cmu_franka_exploration_dataset_converted_externally_to_rlds', 'ucsd_kitchen_dataset_converted_externally_to_rlds', 'ucsd_pick_and_place_dataset_converted_externally_to_rlds', 'austin_sailor_dataset_converted_externally_to_rlds', 'austin_sirius_dataset_converted_externally_to_rlds', 'bc_z', 'usc_cloth_sim_converted_externally_to_rlds', 'utokyo_pr2_opening_fridge_converted_externally_to_rlds', 'utokyo_pr2_tabletop_manipulation_converted_externally_to_rlds', 'utokyo_saytap_converted_externally_to_rlds', 'utokyo_xarm_pick_and_place_converted_externally_to_rlds', 'utokyo_xarm_bimanual_converted_externally_to_rlds', 'robo_net', 'berkeley_mvp_converted_externally_to_rlds', 'berkeley_rpt_converted_externally_to_rlds', 'kaist_nonprehensile_converted_externally_to_rlds', 'stanford_mask_vit_converted_externally_to_rlds', 'tokyo_u_lsmo_converted_externally_to_rlds', 'dlr_sara_pour_converted_externally_to_rlds', 'dlr_sara_grid_clamp_converted_externally_to_rlds', 'dlr_edan_shared_control_converted_externally_to_rlds', 'asu_table_top_converted_externally_to_rlds', 'stanford_robocook_converted_externally_to_rlds', 'eth_agent_affordances', 'imperialcollege_sawyer_wrist_cam', 'iamlab_cmu_pickup_insert_converted_externally_to_rlds', 'uiuc_d3field', 'utaustin_mutex', 'berkeley_fanuc_manipulation', 'cmu_food_manipulation', 'cmu_play_fusion', 'cmu_stretch', 'berkeley_gnm_recon', 'berkeley_gnm_cory_hall', 'berkeley_gnm_sac_son']
     display_key = 'image'
     
     b = tfds.builder_from_directory(builder_dir=dataset2path(dataset))
@@ -138,5 +140,5 @@ def visiual_dataset():
 
 
 if __name__ == "__main__":
-    #visiual_dataset()
-    download_dataset()
+    visiual_dataset()
+    #download_dataset()
